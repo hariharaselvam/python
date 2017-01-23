93a4706307e9e927e95681782d51ac7f51b47fa0
hariharaselvam <hariharaselvam8@gmail.com>
2017-01-23 18:56:40 +0530

    pexpect example

--- day3_regex/re1_sumof.py
+++ day3_regex/re1_sumof.py
@@ -2,8 +2,8 @@ import re
 print "Problem # 1 - regular expression"
 print "The sum of numbers in a string"
 s=raw_input("Enter a string with numbers ")
-pat=raw_input("Enter the patten : ")
-#pat=r"\b\d+\b"
+#pat=raw_input("Enter the patten : ")
+pat=r"\b\d+\b"
 list_of_numbers=re.findall(pat,s)
 result=0
 print list_of_numbers
--- day3_regex/re2_word_with_vowel.py
+++ day3_regex/re2_word_with_vowel.py
@@ -2,8 +2,8 @@ import re
 print "Problem # 2 - regular expression"
 print "Atleast one vowels in a word"
 s=raw_input("Enter a string : ")
-pat=raw_input("Enter the patten : ")
-#pat="\w*[aeiouAEIOU]\w*"
+#pat=raw_input("Enter the patten : ")
+pat="\w*[aeiouAEIOU]\w*"
 
 words=re.findall(pat,s)
 print "The list of words having atleast one vowels"
